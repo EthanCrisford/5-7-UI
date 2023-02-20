11c9240ac93238b1172f12f07d6c27f366e8308b
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2023-02-20 10:53:38 +0000

    1

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour;
+
+  [SerializeField] Slider volumeSlider;
+    [System.Serializable]
+public class Sound
+
+{
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(1f, 3f)]
+    public float pitch;
+
+    [HideInInspector]
+    public AudioSource source;
+}
+
+void Start()
+{
+    {
+        if (!PlayerPrefs.HasKey("musicVol"))
+        {
+            PlayerPrefs.SetFloat("musicVol", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+}
+void Awake()
+{
+    GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+    if (musicObj.Length > 1)
+    {
+        Destroy(this.gameObject);
+        AudioListener.volume = volumeSlider.value;
+
+    }
+    DontDestroyOnLoad(this.gameObject);
+}
+
+public void ChangeVol()
+{
+    AudioListener.volume = volumeSlider.value;
+    Save();
+
+}
+private void Save()
+{
+    PlayerPrefs.SetFloat("musicVol", volumeSlider.value);
+}
+private void Load()
+{
+    volumeSlider.value = PlayerPrefs.GetFloat("musicVol");
+}
+}
