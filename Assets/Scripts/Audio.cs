da92061065ec6f72c99162bd01466eb963d149cc
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2023-02-20 11:02:52 +0000

    1

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -1,65 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
-public class Audio : MonoBehaviour;
-
-  [SerializeField] Slider volumeSlider;
+public class Audio : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
     [System.Serializable]
-public class Sound
 
-{
-    public string name;
-
-    public AudioClip clip;
-
-    [Range(0f, 1f)]
-    public float volume;
-    [Range(1f, 3f)]
-    public float pitch;
-
-    [HideInInspector]
-    public AudioSource source;
-}
-
-void Start()
-{
+    public class Sound
     {
-        if (!PlayerPrefs.HasKey("musicVol"))
+        public string name;
+
+        public AudioClip clip;
+
+        [Range(0f, 1f)]
+        public float volume;
+        [Range(1f, 3f)]
+        public float pitch;
+
+        [HideInInspector]
+        public AudioSource source;
+    }
+
+    void Start()
+    {
         {
-            PlayerPrefs.SetFloat("musicVol", 1);
-            Load();
-        }
-        else
-        {
-            Load();
+            if (!PlayerPrefs.HasKey("musicVol"))
+            {
+                PlayerPrefs.SetFloat("musicVol", 1);
+                Load();
+            }
+            else
+            {
+                Load();
+            }
         }
     }
-}
-void Awake()
-{
-    GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
-    if (musicObj.Length > 1)
+
+    void Awake()
     {
-        Destroy(this.gameObject);
+        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+        if (musicObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+            AudioListener.volume = volumeSlider.value;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void ChangeVol()
+    {
         AudioListener.volume = volumeSlider.value;
-
+        Save();
     }
-    DontDestroyOnLoad(this.gameObject);
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVol", volumeSlider.value);
+    }
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVol");
+    }
 }
 
-public void ChangeVol()
-{
-    AudioListener.volume = volumeSlider.value;
-    Save();
-
-}
-private void Save()
-{
-    PlayerPrefs.SetFloat("musicVol", volumeSlider.value);
-}
-private void Load()
-{
-    volumeSlider.value = PlayerPrefs.GetFloat("musicVol");
-}
-}
