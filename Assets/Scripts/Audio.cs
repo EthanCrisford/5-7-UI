04016d25d7167031ac4b27e2af4b861e3a96b51a
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2023-02-20 14:55:08 +0000

    1

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class Audio : MonoBehaviour
 {
     [SerializeField] Slider volumeSlider;
+    [SerializeField] Slider sfxSlider;
     [System.Serializable]
 
     public class Sound
@@ -24,44 +25,64 @@ public class Audio : MonoBehaviour
         public AudioSource source;
     }
 
+    public AudioSource audioSource;
+    public AudioClip clip;
+
     void Start()
     {
+        if (!PlayerPrefs.HasKey("musicVol"))
         {
-            if (!PlayerPrefs.HasKey("musicVol"))
-            {
-                PlayerPrefs.SetFloat("musicVol", 1);
-                Load();
-            }
-            else
-            {
-                Load();
-            }
+            PlayerPrefs.SetFloat("musicVol", 1);
         }
+
+        if (!PlayerPrefs.HasKey("sfxVol"))
+        {
+            PlayerPrefs.SetFloat("sfxVol", 1);
+        }
+
+        Load();
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Update()
+    {
+        Save();
     }
 
     void Awake()
     {
-        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
-        if (musicObj.Length > 1)
-        {
-            Destroy(this.gameObject);
-            AudioListener.volume = volumeSlider.value;
-        }
-        DontDestroyOnLoad(this.gameObject);
+//AudioListener.volume = volumeSlider.value;
+        
+        //DontDestroyOnLoad(this.gameObject);
     }
 
     public void ChangeVol()
     {
         AudioListener.volume = volumeSlider.value;
-        Save();
+        
     }
+
+    public void ChangeSFXVol()
+    {
+        //AudioListener.volume = volumeSlider.value;
+
+        audioSource.PlayOneShot(clip, sfxSlider.value);
+
+    }
+
     private void Save()
     {
         PlayerPrefs.SetFloat("musicVol", volumeSlider.value);
+        PlayerPrefs.SetFloat("sfxVol", sfxSlider.value);
     }
     private void Load()
     {
         volumeSlider.value = PlayerPrefs.GetFloat("musicVol");
+        sfxSlider.value = PlayerPrefs.GetFloat("sfxVol");
+
+        print("sfx vol=" + PlayerPrefs.GetFloat("sfxVol"));
     }
+
 }
 
